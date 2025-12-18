17dc938c2b0a040f635b5fb2fe0f8360a256f100
Wes Anderson <wesley.herb.anderson@gmail.com>
2025-12-18 07:48:06 -0800

    feat: introduce http server (#10)

---
+++ internal/server/http/options.go
@@ -0,0 +1,21 @@
+package http
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/w-h-a/gomento/internal/server"
+)
+
+type middlewareKey struct{}
+
+func WithMiddleware(ms ...func(h http.Handler) http.Handler) server.Option {
+	return func(o *server.Options) {
+		o.Context = context.WithValue(o.Context, middlewareKey{}, ms)
+	}
+}
+
+func GetMiddlewareFrom(ctx context.Context) ([]func(h http.Handler) http.Handler, bool) {
+	ms, ok := ctx.Value(middlewareKey{}).([]func(h http.Handler) http.Handler)
+	return ms, ok
+}
