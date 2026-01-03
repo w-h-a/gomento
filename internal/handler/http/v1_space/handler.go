1f5be249fe63372df58e8654a970fa2446c7663e
Wes Anderson <wesley.herb.anderson@gmail.com>
2026-01-03 08:49:31 -0800

    feat: enable retrieving tasks by space at handler layer

--- internal/handler/http/v1_space/handler.go
+++ internal/handler/http/v1_space/handler.go
@@ -2,10 +2,13 @@ package v1space
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 
 	"github.com/google/uuid"
+	"github.com/gorilla/mux"
 	httphandler "github.com/w-h-a/gomento/internal/handler/http"
+	"github.com/w-h-a/gomento/internal/service"
 	v1space "github.com/w-h-a/gomento/internal/service/v1_space"
 	"github.com/w-h-a/gomento/internal/util"
 )
@@ -43,6 +46,38 @@ func (h *v1Handler) Create(w http.ResponseWriter, r *http.Request) {
 	}
 
 	httphandler.WrtJSON(w, http.StatusCreated, s)
+}
+
+func (h *v1Handler) ListTasks(w http.ResponseWriter, r *http.Request) {
+	traceId := httphandler.GetTraceId(r)
+	ctx := util.WithTraceId(r.Context(), traceId)
+
+	vars := mux.Vars(r)
+	spaceId, err := uuid.Parse(vars["space_id"])
+	if err != nil {
+		httphandler.WrtErr(w, http.StatusBadRequest, "invalid space id")
+		return
+	}
+
+	var status *string
+	if s := r.URL.Query().Get("status"); s != "" {
+		status = &s
+	}
+
+	out, err := h.service.ListTasks(ctx, v1space.ListTasksInput{
+		SpaceId: spaceId,
+		Status:  status,
+	})
+	if err != nil {
+		if errors.Is(err, service.ErrSpaceNotFound) {
+			httphandler.WrtErr(w, http.StatusNotFound, "Space not found")
+			return
+		}
+		httphandler.WrtErr(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	httphandler.WrtJSON(w, http.StatusOK, out)
 }
 
 func NewV1Handler(s *v1space.V1Service) *v1Handler {
