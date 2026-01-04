2c2adb1d5981994427c2c78edbaeb1c6bd8f8c4d
Wes Anderson <wesley.herb.anderson@gmail.com>
2026-01-04 07:41:42 -0800

    chore: adding todos for future work

--- internal/client/persister/persister.go
+++ internal/client/persister/persister.go
@@ -21,10 +21,13 @@ type V1Persister interface {
 	CreateProject(ctx context.Context, proj *v1.Project) error
 
 	CreateSpace(ctx context.Context, space *v1.Space) error
+	// TODO: List Spaces
 	GetSpace(ctx context.Context, id uuid.UUID) (*v1.Space, error)
 	SaveSkill(ctx context.Context, skill *v1.Skill) error
+	// TODO: Search Skills
 
 	CreateSession(ctx context.Context, sess *v1.Session) error
+	// TODO: List Sessions
 	GetSession(ctx context.Context, id uuid.UUID) (*v1.Session, error)
 	UpdateSession(ctx context.Context, sess *v1.Session) error
 
@@ -36,11 +39,12 @@ type V1Persister interface {
 
 	CreateMessageWithAssets(ctx context.Context, msg *v1.Message, assets map[int]*v1.Asset) error
 	ListMessages(ctx context.Context, sessionId uuid.UUID, opts ...ListMessagesOption) ([]v1.Message, error)
-	GetAssets(ctx context.Context, ids []uuid.UUID) (map[uuid.UUID]*v1.Asset, error)
+	// TODO: Search Messages
 
 	CreateArtifact(ctx context.Context, a *v1.Artifact) error
 	UpsertFileWithAsset(ctx context.Context, file *v1.File, asset *v1.Asset) error
 	ListArtifacts(ctx context.Context, projectId uuid.UUID) ([]v1.Artifact, error)
 	ListFiles(ctx context.Context, artifactId uuid.UUID, opts ...ListFilesOption) ([]v1.File, error)
 	GetFile(ctx context.Context, artifactId uuid.UUID, path string, filename string) (*v1.File, error)
+	GetAssets(ctx context.Context, ids []uuid.UUID) (map[uuid.UUID]*v1.Asset, error)
 }
