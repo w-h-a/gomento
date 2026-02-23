045ed851208389e4d7121aca81a35acd858841d5
Wes Anderson <wesley.herb.anderson@gmail.com>
2026-02-23 07:34:00 -0800

    chore: rm unused example/prototype agent code

--- api/chat/v1/chat.go
+++
@@ -1,9 +0,0 @@
-package v1
-
-import "github.com/google/uuid"
-
-type Chat struct {
-	SessionId uuid.UUID         `json:"session_id"`
-	Text      string            `json:"text"`
-	Files     map[string]string `json:"files"`
-}
